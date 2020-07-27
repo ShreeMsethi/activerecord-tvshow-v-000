1d23972ed401e1b593b007f8976474a71629b1f9
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-07-27 03:59:02 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    Show.where(self.highest_rating)
+    Show.
   end 
   
   def self.lowest_rating
@@ -23,5 +23,9 @@ class Show < ActiveRecord::Base
   
   def self.popular_shows
     Show.where("rating > 5", [])
+  end 
+  
+  def self.shows_by_alphabetical_order
+    Show.order
   end  
 end
